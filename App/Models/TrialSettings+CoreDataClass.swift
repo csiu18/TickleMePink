58b219ff396b887d607a852ada76ef96135b98d6
Anthony Tranduc <anthonytranduc@gmail.com>
2022-03-10 03:21:36 -0500

    implemented saving partial trial settings to core data.

---
+++ App/Models/TrialSettings+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  TrialSettings+CoreDataClass.swift
+//  App
+//
+//  Created by Anthony Tranduc on 3/8/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TrialSettings)
+public class TrialSettings: NSManagedObject {
+
+}
--- App/Views/CreateTrialSettingsView.swift
+++ App/Views/CreateTrialSettingsView.swift
@@ -6,10 +6,41 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct CreateTrialSettingsView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @State private var partCondition:String = ""
+    @State private var screens:[Int64] = []
+    
+    
     var body: some View {
-        Text("Create Trial Settings Screen")
+        VStack(spacing: 20){
+        Text("Create Trial Settings")
+            VStack(alignment: .leading) {
+                Text("Participant Condition")
+                TextField("Enter Participant Condition...", text:$partCondition)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.bottom, 50)
+            
+                Text("Trial Sequence")
+                
+            
+            }
+            Spacer()
+            Button("Save Sequence", action:saveSequence)
+        }
+    }
+    
+    func saveSequence() {
+        let newSettings = TrialSettings(context: self.viewContext)
+        newSettings.partCondition = self.partCondition
+        do {
+            try self.viewContext.save()
+        } catch {
+            print("Error in saving sequence: \(error.localizedDescription)")
+        }
     }
 }
 
