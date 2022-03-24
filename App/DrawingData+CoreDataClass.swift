f720038dda014bca70d6c32440d9d3873d2e1241
Samuel Chan <sauch168@gmail.com>
2022-03-24 17:45:02 -0400

    Ability to save drawing data and export it

---
+++ App/DrawingData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  DrawingData+CoreDataClass.swift
+//  App
+//
+//  Created by Sam Chan on 3/11/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(DrawingData)
+public class DrawingData: NSManagedObject {
+
+}
