cec22b7c27b589e899cc8ea206334d27b012c833
cgeary123 <51299962+cgeary123@users.noreply.github.com>
2022-03-27 12:39:21 -0400

    Update UploadMediaView.swift

--- App/UploadMediaView.swift
+++ App/UploadMediaView.swift
@@ -7,14 +7,27 @@
 
 import SwiftUI
 import UIKit
+import CoreData
+import Combine
 
 struct UploadMediaView: View {
+    //var mutArray : NSMutableArray = []
     
     // show image picker
     @State var showImagePicker: Bool = false
     
+    @State var videoPicker: VideoPicker!
+    
     //show selected image
     @State var selectedImage: Image? = Image("")
+    
+    @State var mediaURL: NSURL?
+    
+    // get the context
+    @Environment(\.managedObjectContext) private var viewContext
+    // get the fetch request
+    @FetchRequest(entity: Media.entity(), sortDescriptors: [])
+        var configs: FetchedResults<Media>
     
     var body: some View {
         VStack{
@@ -22,21 +35,30 @@ struct UploadMediaView: View {
             Button(action: {
                 self.showImagePicker.toggle()
             }, label: {
-                Text("Upload Media")
+                Text("Upload Image")
             })
             
             // show image
             self.selectedImage?.resizable().scaledToFit()
+            let _ = print("This be the URL")
+            let _ = print(mediaURL)
             
             //show button to upload iamge
             Button(action: {
-                //
+                let uiImage: UIImage = self.selectedImage.asUIImage()
+                let imageData = uiImage.jpegData(compressionQuality: 1.0)
+                //let persistenceController = PersistenceController.shared
+                
+                //let entityName = NSEntityDescription.entity(forEntityName: "Media", in: viewContext)
+                let imageInstance = Media(context: viewContext)
+                imageInstance.data = imageData
+           
             }, label: {
                 Text("Save Media")
             })
         }
         .sheet(isPresented: $showImagePicker, content: {
-            ImagePicker(image: self.$selectedImage)
+            ImagePicker(image: self.$selectedImage, url: self.$mediaURL)
         })
     }
 }
@@ -46,4 +68,3 @@ struct UploadMedia_Previews: PreviewProvider {
         UploadMediaView()
     }
 }
-
